f5bff380ac90bf3db098e4b689e4a09b9cedae33
Francisco <fgsj@cin.ufpe.br>
2021-03-26 23:27:08 -0300

    Code for calculate four notes and average

---
+++ Python_Sequential/Code_Four.py
@@ -0,0 +1,16 @@
+"""
+Author: Francisco Gomes
+"""
+
+#Received values of user
+noteOne = float(input("Enter with note one: "))
+noteTwo = float(input("Enter with note Two: "))
+noteThree = float(input("Enter with note Three: "))
+noteFour = float(input("Enter with note Four: "))
+
+#Calculating operation
+average = (noteOne + noteTwo + noteThree + noteFour) / 4;
+
+#printing average
+print("Average of notes is: ",average)
+
