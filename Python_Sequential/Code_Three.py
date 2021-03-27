0a6e4ea6b8027359ff61c3a43cf9cdb766e03631
Francisco <fgsj@cin.ufpe.br>
2021-03-26 23:16:57 -0300

    Calculating sum of numbers

--- Python_Sequential/Code_Three.py
+++ Python_Sequential/Code_Three.py
@@ -2,4 +2,13 @@
 Author: Francisco Gomes
 """
 
+#Received values
+numberOne = int(input("Enter with first number: "))
+numberTwo = int(input("Enter with second number: "))
+
+#Calculating operation Math
+sumNumbers = (numberOne + numberTwo);
+
+#Printing values
+print("Sum of numbers is: ",sumNumbers);
 
