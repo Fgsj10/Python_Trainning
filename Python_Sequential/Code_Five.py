33033ab9b838c18bc83855bb75ebca3957c03617
Francisco <fgsj@cin.ufpe.br>
2021-03-26 23:34:35 -0300

    Value converted meters in centimeters with 2 decimal

---
+++ Python_Sequential/Code_Five.py
@@ -0,0 +1,20 @@
+"""
+Author: Francisco Gomes
+"""
+
+#Received values of user
+meters = float(input("Enter with value em meters: "))
+
+#Converting value meters in centimeters
+
+converter = (meters * 100)
+
+#printing value converted
+print("Value meters converted is: %.2f " %(converter), "cm") #Printing value with 2 decimal value
+
+
+
+
+
+
+
