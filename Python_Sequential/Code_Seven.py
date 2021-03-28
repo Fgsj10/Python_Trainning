f4ba7e62460f4484a1e27a45e5d1460889ebf3c9
Francisco <fgsj@cin.ufpe.br>
2021-03-27 23:18:35 -0300

    Calculating area of squat

---
+++ Python_Sequential/Code_Seven.py
@@ -0,0 +1,18 @@
+"""
+Author: Francisco Gomes
+"""
+
+#Variables
+
+side = float(input("Enter with value of side: "))
+
+#Calculating area of squat
+
+areaSquat = (side ** 2);
+
+
+#printing value of area squat
+
+print("Area squat is: %.2f" %(areaSquat)," cm² ")
+
+
