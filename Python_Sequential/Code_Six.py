05e58580f6a21df06dcc6c0b0a6919db7e955d5c
Francisco <fgsj@cin.ufpe.br>
2021-03-27 23:09:20 -0300

    Calculating area of circle

---
+++ Python_Sequential/Code_Six.py
@@ -0,0 +1,17 @@
+"""
+Author: Francisco Gomes
+"""
+
+pi = 3.14
+
+#Value received of user
+raidCircle = int(input("Enter with value of raid circle: "))
+
+
+#Calculating operation of area circle
+areaCircle = pi * (raidCircle ** 2)
+
+
+#Printing value final of area circle
+print("Area of circle is: ", (areaCircle))
+
