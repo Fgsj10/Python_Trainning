8b9fc0836559ae86331c7b28f66be3c4fb27385f
Francisco <fgsj@cin.ufpe.br>
2021-03-29 23:32:36 -0300

    Calculating weight ideal

---
+++ Python_Sequential/Code_Eleven.py
@@ -0,0 +1,16 @@
+"""
+Author: Francisco Gomes
+"""
+
+
+#Creating variables for received data inputs
+height = float(input("Enter with your height: "))
+
+#Calculating weight ideal
+weightIdeal = (72.7 * height) - 58
+
+#Printing weight ideal
+print("Weight ideal is: %.2f" %(weightIdeal), "kg")
+
+
+
