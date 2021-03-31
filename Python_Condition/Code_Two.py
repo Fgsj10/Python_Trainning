a02a7ae349ee731bfc81753a5b68bab762cd8e06
Francisco <fgsj@cin.ufpe.br>
2021-03-31 00:42:00 -0300

    Checking if value is negative or positive

---
+++ Python_Condition/Code_Two.py
@@ -0,0 +1,14 @@
+"""
+Author: Francisco Gomes
+"""
+
+
+#Creating variables and user input data
+number = int(input("Enter with number: "))
+
+#Structure condition for checking value
+
+if(number > 0):
+    print("number positive")
+else:
+    print("number negative")
