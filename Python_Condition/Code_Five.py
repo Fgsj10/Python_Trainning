be3199ca85a4267038ad57261b953496db69a1d7
Francisco <fgsj@cin.ufpe.br>
2021-03-31 00:57:20 -0300

    Calculating average with aproved or reproved

---
+++ Python_Condition/Code_Five.py
@@ -0,0 +1,31 @@
+"""
+Author: Francisco Gomes
+"""
+
+
+#creating variables and data user input received
+
+noteOne = float(input("Enter with first note: "))
+noteTwo = float(input("Enter with second note: "))
+
+#Calculating average
+average = (noteOne + noteTwo) / 2;
+
+print("You average is partial %.2f " %(average))
+
+#Checking values of average
+if(average == 10.0):
+    print("Approved with distinction")
+    print("Average is %.2f " %(average))
+elif(average < 7.0 and average > 0):
+    print("Reproved")
+    print("Average is: %.2f " %(average))
+elif(average >= 7 and average < 9.9):
+    print("Approved")
+    print("Average is: %.2f " %(average))
+else:
+    print("Average is invalid")
+    print("Average  < 0")
+
+
+
