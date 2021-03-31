59f9fba1ee5123855d69d2cb588360bd3ac00483
Francisco <fgsj@cin.ufpe.br>
2021-03-30 23:53:13 -0300

    Identify bigger number structure condition

---
+++ Python_Condition/Code_One.py
@@ -0,0 +1,22 @@
+"""
+Author: Francisco Gomes
+"""
+
+
+#Received numbers and input data user
+numberOne = int(input("Enter with number one: "))
+numberTwo = int(input("Enter with number two: "))
+
+
+#Creating structure condition for bigger number
+
+if(numberOne > numberTwo):
+    print("Number one is bigger")
+    print(numberOne)
+else:
+    print("Number two is bigger")
+    print(numberTwo)
+
+
+
+
