95370ea355733f7b554c7d83e225c79c69a178f8
Francisco <fgsj@cin.ufpe.br>
2021-04-06 22:06:09 -0300

    Calculating sum and average of numbers

---
+++ Python_Repetition/Code_Six.py
@@ -0,0 +1,30 @@
+"""
+Author = Francisco Junior
+"""
+
+
+#Creating variables and received user input data
+sum = 0
+for x in range(0,5):
+    number = int(input("Enter with a number: "))
+    sum += number
+
+
+#Printing sum of numbers
+
+print("Sum of numbers is: %.2f " %(sum))
+
+#Calculating average of numbers
+
+average = sum / 5
+
+#Printing average
+
+print("Average of numbers is: %.2f " %(average))
+
+
+
+
+
+
+
