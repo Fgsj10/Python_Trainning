002a4cb3982e2263793112fc16cbaaddcbcb524a
Francisco <fgsj@cin.ufpe.br>
2021-04-06 22:22:45 -0300

    Calculating numbers final of between user enter two numbers

---
+++ Python_Repetition/Code_Nine.py
@@ -0,0 +1,20 @@
+"""
+Author = Francisco Junior
+"""
+
+#Variables and received user data input
+numberOne = int(input("Enter first number: "))
+numberTwo = int(input("Enter second number: "))
+sumNumbers = 0
+
+for x in range(numberOne, numberTwo):
+    sumNumbers += x
+
+
+#Calculating sum of numbers
+print("Sum of numbers is: %d " %(sumNumbers))
+
+
+
+
+
