3a0d67189bc80878ac4af7a9d0c58b556f0ff4c1
Francisco <fgsj@cin.ufpe.br>
2021-04-06 22:12:47 -0300

    Calculating numbers odd of between

---
+++ Python_Repetition/Code_Seven.py
@@ -0,0 +1,15 @@
+"""
+Author = Francisco Junior
+"""
+
+
+#Creating variables and received input data user
+#Printing numbers pair
+
+for x in range(0,50):
+    if(x % 2 == 1):    #Calculating numbers odd
+        odd = x
+        print(odd)
+
+
+
