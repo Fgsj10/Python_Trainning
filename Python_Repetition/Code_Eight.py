50a06feaa4adbc0306d49413403fe93f320543a4
Francisco <fgsj@cin.ufpe.br>
2021-04-06 22:17:16 -0300

    Printing numbers of between two numbers input user

---
+++ Python_Repetition/Code_Eight.py
@@ -0,0 +1,14 @@
+"""
+Author = Francisco Junior
+"""
+
+#Variables received data user input
+numberOne = int(input("Enter first number: "))
+numberTwo = int(input("Enter second number: "))
+
+
+#Printing numbers of between
+for x in range(numberOne, numberTwo):
+    print(x + 1)
+
+
