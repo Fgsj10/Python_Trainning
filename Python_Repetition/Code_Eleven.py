a4cf72bba5d503dbe5c9614cb945f8563e5696d5
Francisco <fgsj@cin.ufpe.br>
2021-04-07 23:28:27 -0300

    Calculating numbers of base and potential

---
+++ Python_Repetition/Code_Eleven.py
@@ -0,0 +1,18 @@
+"""
+Author = Francisco Junior
+"""
+
+#Variables and received user data input
+base = int(input("Enter with number base: "))
+potential = int(input("Enter with number potential: "))
+
+#Calculating number and potential
+calculatePotential = (base ** potential)
+
+#printing result final
+
+print("Number potential is: %d" %(calculatePotential))
+
+
+
+
