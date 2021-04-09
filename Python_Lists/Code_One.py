945158fc125346d067545382352e90d3f46b4387
Francisco <fgsj@cin.ufpe.br>
2021-04-08 21:53:24 -0300

    Creating list of five numbers and printing list

---
+++ Python_Lists/Code_One.py
@@ -0,0 +1,22 @@
+"""
+Author = Francisco Junior
+"""
+
+#Variables and user input data received
+numberList = []
+
+#Structure repetition with adding number in list
+for x in range(0,5):
+    number = int(input("Enter with a number: "))
+    numberList.append(number) #Adding number in list
+
+
+#Printing final list of numbers
+
+print("A list of numbers is: ", (numberList))
+
+
+
+
+
+
