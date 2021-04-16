b6d06e5985affea4c8888cc46559ca3b4f2a510f
Francisco <fgsj@cin.ufpe.br>
2021-04-15 22:37:04 -0300

    Code incomplete for questions in crime and detective

---
+++ Python_Lists/Code_Ten.py
@@ -0,0 +1,18 @@
+"""
+Author = francisco Junior
+"""
+
+#Input and data received user
+
+sumAnswerYes = 0
+
+for x in range(0,1):
+    print("Calling for victim?")
+    print("You here in local of crime?")
+    print("You living in local less?")
+    print("You should for victim?")
+    print("You worked with victim?")
+
+#list for adding answer of questions
+answer = []
+
