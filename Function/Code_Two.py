eac5d9ac9555fad9149823d2377c7516d46796c0
Francisco <fgsj@cin.ufpe.br>
2021-04-16 23:07:53 -0300

    Code modified of function sum numbers

--- Function/Code_Two.py
+++ Function/Code_Two.py
@@ -5,10 +5,14 @@ Author = Francisco Junior
 #Creating function
 
 def sumNumbers(n1,n2,n3):
-    sum = 0
     numbersSum = n1+n2+n3
-    print(numbersSum)
+    print("Sum of numbers is:", numbersSum)
+
+#Input data user received
+numberOne = int(input("Number One: "))
+numberTwo = int(input("Number Two: "))
+numberThree = int(input("Number Three: "))
 
 
 #Printing numbers
-sumNumbers(10,10,10)
+sumNumbers(numberOne, numberTwo, numberThree)
