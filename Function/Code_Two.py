6c6087adaa858850671c080039967230927159c0
Francisco <fgsj@cin.ufpe.br>
2021-04-16 22:54:20 -0300

    Code function of sum numbers and three parameters

---
+++ Function/Code_Two.py
@@ -0,0 +1,14 @@
+"""
+Author = Francisco Junior
+"""
+
+#Creating function
+
+def sumNumbers(n1,n2,n3):
+    sum = 0
+    numbersSum = n1+n2+n3
+    print(numbersSum)
+
+
+#Printing numbers
+sumNumbers(10,10,10)
