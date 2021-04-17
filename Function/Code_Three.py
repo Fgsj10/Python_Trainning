fdd32c2864f790f74e1a7b11ac07d93770c4d388
Francisco <fgsj@cin.ufpe.br>
2021-04-16 23:20:34 -0300

    Function of number positive or negative

---
+++ Function/Code_Three.py
@@ -0,0 +1,20 @@
+"""
+Author = Francisco Junior
+"""
+
+#Creating function
+
+def numberPositiveNegative(n1):
+    if n1 > 0:
+        print("Number positive")
+    else:
+        print("Number negative")
+
+#Received input data user
+number = int(input("Enter number one: "))
+
+
+#Printing number with function
+numberPositiveNegative(number)
+
+
