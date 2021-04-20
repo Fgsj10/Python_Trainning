58c27dba5e07a493a646a9c9f6daafe1d3a15768
Francisco <fgsj@cin.ufpe.br>
2021-04-19 23:11:01 -0300

    Printing value of name reversed

---
+++ Strings/Code_Two.py
@@ -0,0 +1,21 @@
+"""
+Author = Francisco Gomes
+"""
+
+#Variables and input data user
+
+name = str(input("Enter with name, please: "))
+
+#Converting name for maiuscule
+newName = name.upper()
+#Printing name in maiuscule
+print("new format name is: ", newName)
+
+#Method for converting name reverse
+nameReverse = newName[::-1]
+print("Name reverse is: ", nameReverse)
+
+
+
+
+
