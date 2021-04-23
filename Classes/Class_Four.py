ea16a0cd2ff833d13a11ed9a0ca341e6494e0237
Francisco <fgsj@cin.ufpe.br>
2021-04-22 23:38:35 -0300

    Class incomplete of data people

---
+++ Classes/Class_Four.py
@@ -0,0 +1,26 @@
+"""
+Author = Francisco Junior
+"""
+
+#Creating class, methods and attributes
+
+
+class People():
+
+    def __init__(self, name, age, weight, height):
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
+
+    def weightUp(self, weight):
+        self.weight += weight
+
+    def heighter(self, height):
+        self.height += height
+
+    
+
+
+
+
