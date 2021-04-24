162cdb6b114ccc0b2023093a6893917f842685af
Francisco <fgsj@cin.ufpe.br>
2021-04-24 20:29:11 -0300

    Adding comments of explanation code

--- Algorithms/Bubble_Sort.py
+++ Algorithms/Bubble_Sort.py
@@ -7,6 +7,7 @@ Author= Francisco Junior
 def bubblesort(arr):
     n = len(arr)
 
+    #Checking and runner all values in array
     for x in range(n):
         for j in range(n-x-1):
             if arr[j] > arr[j+1]:
@@ -17,8 +18,10 @@ def bubblesort(arr):
 
 arr = [15,18,16,14,9,6,31,47]
 
+#Calling method bubble sort with values
 bubblesort(arr)
 
+#Printing all values sorted
 print("List sorted with algorithm bubble sort")
 
 for x in range(len(arr)):
